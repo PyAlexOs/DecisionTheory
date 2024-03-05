4f04a0d7dfde9fb3c69ca6623c95c69bf41c7402
pyalexos <aligos@yandex.ru>
2024-03-05 17:46:44 +0300

    border optimization

--- Pareto.py
+++ Pareto.py
@@ -3,16 +3,16 @@ KEYS = ["Стоимость машины",
         "Вместительность багажника",
         "Мощность двигателя",
         "Клиренс"]
-TABLE = [['Mazda CX-5', 4.35, 7.1, 442, 156, 210],
-         ['Toyota Land Cruiser Prado', 4.32, 11.0, 621, 163, 215],
-         ['Audi A4', 3.99, 7.2, 495, 245, 180],
-         ['Kia Sportage', 3.13, 8.2, 540, 150, 181],
-         ['Volvo XC90', 5.58, 5.7, 721, 249, 238],
-         ['Subaru Outback', 5.63, 7.3, 522, 188, 213],
-         ['Nissan X-Trail', 2.21, 5.3, 497, 130, 210],
-         ['Mercedes GLC', 5.12, 5.5, 620, 197, 180],
-         ['Jeep Wrangler', 4.92, 11.3, 142, 272, 277],
-         ['УАЗ Патриот', 1.94, 11.5, 1130, 150, 210]]
+TABLE = [['Mazda CX-5', 4.2, 7.1, 565, 165, 240],
+         ['Toyota Land Cruiser Prado', 4.3, 11.0, 550, 163, 235],
+         ['Audi A4', 4.1, 7.1, 570, 245, 245],
+         ['Kia Sportage', 3.2, 8.2, 540, 150, 181],
+         ['Volvo XC90', 5.6, 5.7, 721, 249, 235],
+         ['Subaru Outback', 5.6, 7.3, 522, 188, 213],
+         ['Nissan X-Trail', 2.2, 5.3, 497, 225, 230],
+         ['Mercedes GLC', 5.1, 5.5, 620, 197, 180],
+         ['Jeep Wrangler', 4.9, 11.3, 142, 225, 220],
+         ['УАЗ Патриот', 1.9, 11.2, 1130, 225, 225]]
 
 
 def main():
@@ -27,27 +27,81 @@ def main():
     dataframe.normalize(aspirations)
     dataframe.show("Normalized")
 
-    dataframe.optimize()
+    elements = dataframe.optimize()
+    optimized = dataframe.iloc[elements]
+    optimized.normalize(aspirations)
+    optimized.show("Optimized")
+
+    elements = dataframe.optimize()
+    optimized = dataframe.iloc[elements]
+    optimized.normalize(aspirations)
+    elements = optimized.border_optimize({"bigger": [[1, 1]]})
+    if len(elements) > 0:
+        border_optimized = optimized.iloc[elements]
+        border_optimized.show("Border optimized")
+    else:
+        print("Failed to optimize the set using boundaries")
 
 
-def optimize(self):
-    def compare() -> list[list[...]]:
+def border_optimize(self, borders: dict[str, list[...]]) -> list[str]:
+    """ Narrows down the set of optimal options using borders """
+    elements = set()
+
+    for (key, value) in borders.items():
+        if key not in ["bigger", ">", "smaller", "<", "equals", "="]:
+            raise KeyError('The keys can only be as follows: ["bigger", ">", "smaller", "<", "equals", "="]')
+
+        if not isinstance(value, list) or len(value) == 0:
+            raise ValueError('The value must be a list of two numbers [index, condition]')
+
+        for condition in value:
+            if (not isinstance(condition, list) or
+                    len(condition) != 2 or
+                    condition[0] not in range(0, self.shape[0]) or
+                    not all([i.isdigit() or i == "." for i in str(condition[1])])):
+                raise ValueError('The value must be a list of two numbers [index, condition]')
+
+            for row in range(self.shape[0]):
+                if key in ["bigger", ">"]:
+                    print(self.index)
+                    print(row, condition[0])
+                    print(self.loc[row, condition[0]])
+                    if self.loc[row, condition[0]] > condition[1]:
+                        elements.add(row)
+                elif key in ["smaller", "<"]:
+                    if self.loc[row, condition[0]] < condition[1]:
+                        elements.add(row)
+                else:
+                    if self.loc[row, condition[0]] == condition[1]:
+                        elements.add(row)
+
+    return list(elements)
+
+
+def optimize(self) -> list[str]:
+    """ Finds the optimal Pareto set """
+    elements = set()
+
+    def compare() -> list[list[str]]:
         result = [["x" for _ in range(self.shape[0])] for _ in range(self.shape[0])]
         for alt_1 in range(self.shape[0]):
             for alt_2 in range(alt_1 + 1, self.shape[0]):
                 if (self.iloc[alt_1].values >= self.iloc[alt_2].values).all():
                     result[alt_2][alt_1] = self.index.values[alt_1]
+                    elements.add(alt_1)
                 elif (self.iloc[alt_1].values <= self.iloc[alt_2].values).all():
                     result[alt_2][alt_1] = self.index.values[alt_2]
+                    elements.add(alt_2)
                 else:
                     result[alt_2][alt_1] = "н"
-
         return result
 
     compared = pd.DataFrame(compare(),
                             columns=[f'A{i}' for i in range(1, 11)],
                             index=[f'A{i}' for i in range(1, 11)])
     compared.show("Pairwise comparison")
+
+    return list(elements)
 
 
 def normalize(self, aspirations: list[bool]):
@@ -58,7 +112,7 @@ def normalize(self, aspirations: list[bool]):
                 self.iloc[row, col] = (1 / self.iloc[row, col])
 
 
-def show(self, header: str):
+def show(self, header: str = ""):
     """ Prints dataframe with header """
     console_width = shutil.get_terminal_size().columns
     filler = "-" * ((console_width - len(header)) // 2)
@@ -67,9 +121,9 @@ def show(self, header: str):
 
 if __name__ == "__main__":
     import pandas as pd
-    import numpy as np
     import shutil
 
+    pd.DataFrame.border_optimize = border_optimize
     pd.DataFrame.optimize = optimize
     pd.DataFrame.normalize = normalize
     pd.DataFrame.show = show
